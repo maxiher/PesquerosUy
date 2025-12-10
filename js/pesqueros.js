107d291f8362273f75b03cc8ce781281327e657c
MaxiHer <maxihernandez444@gmail.com>
2025-12-10 12:07:50 -0300

    Mejora de JSONS y estilos en pesqueros

--- js/pesqueros.js
+++ js/pesqueros.js
@@ -23,52 +23,82 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            const titulo = data[seccion].nombre ;
-            const profundidad = data[seccion].profundidad ;
+            const titulo = data[seccion].nombre;
+            const profundidad = data[seccion].profundidad;
             const enganche = data[seccion].enganche;
+            const foto = data[seccion].imagenes
+            const departamento = data[seccion].departamento
+            const acceso = data[seccion].acceso
+            const latitud = data[seccion].ubicacion.lat
+            const longitud = data[seccion].ubicacion.lng
 
+            
             container.innerHTML = `
     <h1>${titulo}</h1>
-    <img src="Fotos/index${data[seccion].departamento.toLowerCase()}.jpg">
+    <img src="${foto}">
     <p>${data[seccion].descripcion}</p>
-    <table>
-    <th>Caracteristicas</th>
-    <tr><td>Profundidad:</td> <td id="info">${profundidad}</td></tr>
-    <tr><td>Enganche:</td><td>${enganche}</td><tr/>
-    </table>
-    
+
+    <span class="tag" id="acceso">Acceso: ${acceso}</span>
+    <span class="tag" id="profundidad">Profundidad: ${profundidad}</span>
+    <span class="tag" id="enganche">Enganche: ${enganche}</span>
+
+
+    <div class="map">
+        <iframe
+          width="100%"
+          height="200"
+          frameborder="0"
+          src="https://maps.google.com/maps?q=${latitud},${longitud}&z=15&output=embed">
+        </iframe>
+        <p>Lat: ${latitud}, Lng: ${longitud}</p>
+    </div>
     `;
 
-    color("profundidad", profundidad)
-
-
-        })        
+            color('profundidad')
+            color('enganche', true)
+            color('acceso')
+        })
 })
 
-    
-function color(idElemento, valor){
-const elemento = document.querySelector('');
-if (!elemento) return;
 
-const num = Number(valor)
+function color(elementId, invertir = false) {
+    const elemento = document.getElementById(elementId);
 
-elemento.classList.remove("poco", "medio", "mucho");
 
-    if (num > 0 && num < 2) {
-        elemento.classList.add("rojo");
-      } else if (profundidad === 3) {
-        elemento.classList.add("amarillo");
-      } else {
-        elemento.classList.add("verde");
-      }
-    }
+    console.log(elemento)
 
- /*   function escala(numero) {
-    if (numero = 1) {
-        lugar.classList.add("poco");
-        return "Nada"
+    if (!elemento) return;
+
+    const num = Number(elemento.textContent)
+
+    elemento.classList.remove("rojo", "amarillo", "verde", "amarillo", "verdeclaro");
+
+    if (!invertir) {
+        if (num < 2) {
+            elemento.classList.add('rojo');
+        } else if (num === 2) {
+            elemento.classList.add('amarillo');
+        } else if (num === 3) {
+            elemento.classList.add('verdeclaro')
+        } else {
+            elemento.classList.add('verde');
+        }
     } else {
-        return "algo";
+        if (num < 2 || elemento.textContent === "Facil") {
+            elemento.classList.add('verde');
+        } else if (num >= 2 && num < 4 || elemento.textContent === "Medio") {
+            elemento.classList.add('amarillo');
+        } else {
+            elemento.classList.add('rojo');
+        }
+
+    }
+}
+
+/*function paraAcceso(data[seccion].acceso){
+    const elemento = 
+    if (acceso === "Facil") {
+
     }
 }*/
 
